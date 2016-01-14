06fb7e5a9541dd740403463e12a3ef70a9f20066
skopro <kavoosisoroosh@gmail.com>
2016-01-14 11:15:00 +0330

    Add saving capability to increment

--- increment.c
+++ increment.c
@@ -1,8 +1,48 @@
 #include "types.h"
 #include "user.h"
+#include "fcntl.h"
+#include "mmu.h"
+#include "param.h"
+#include "proc.h"
 
 int stdout = 1;
 int stderr = 2;
+
+void save(char* name, struct proc p)
+{
+    printf(stdout, "saving...\n");
+
+    int fd = open(name, O_CREATE|O_RDWR);
+    if(fd >= 0)
+    {
+        printf(stdout, "the file '%s' is created.\n", name);
+    }
+    else
+    {
+        printf(stderr, "error: an error occured while creating the file '%s'.\n", name);
+        exit();
+    }
+
+    if(write(fd, &p, sizeof(p)) == sizeof(p))
+    {
+        printf(stdout, "the process info is written to the file '%s'.\n", name);
+    }
+    else
+    {
+        printf(stderr, "error: an error occured while writing to the file '%s'.\n", name);
+        exit();
+    }
+
+    close(fd);
+}
+
+void stop(void)
+{
+    struct proc* p = malloc(sizeof(struct proc));
+    getproc(p);
+    save("backup", *p);
+    exit();
+}
 
 int main(int argc, char *argv[])
 {
@@ -30,6 +70,11 @@ int main(int argc, char *argv[])
             {
                 printf(stdout, "%d\n", counter);
                 sleep(100);
+
+                if(counter % 5 == 0)
+                {
+                    stop();
+                }
             }
 
             exit();
